b5fbc5dd7c67830a7a2f64966138115d709d6fbf
plani01 <73602353+plani01@users.noreply.github.com>
2025-12-11 14:11:09 -0500

    clean up repo, unused files deleted

--- VidSynth_Pipeline/llm_service/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Set a fast, small default model for local testing
-# MODEL_ID = os.getenv("MODEL_ID", "google/flan-t5-base")
-MODEL_ID = os.getenv("MODEL_ID", "google/flan-t5-small")  # Change to small! for bias
